5a6b72738638abf8d8210a1a5d0a809d4816cc17
oricalvo <ori.calvo@gmail.com>
2015-08-02 16:18:43 +0300

    More

---
+++ Labs/02 Introducing Angular/Starter/Models/Book.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="Group.js" />
+
+var ContactBook = ContactBook || {};
+
+ContactBook.book = (function (Contact, Group) {
+
+    function Book() {
+        this.root = new Group("~");
+    }
+
+    Book.prototype.dump = function () {
+        this.root.dump();
+    }
+
+    return new Book();
+
+})(ContactBook.Contact, ContactBook.Group);
