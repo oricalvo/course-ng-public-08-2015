5be3af5a2150f35f2e33d0737867dd1d31e8b96a
oricalvo <ori.calvo@gmail.com>
2015-08-02 15:51:39 +0300

    More

---
+++ Labs/01 OOP/Solution/Book.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="Group.js" />
+
+var ContactBook = ContactBook || {};
+
+ContactBook.Book = (function (Contact, Group) {
+
+    function Book() {
+        this.root = new Group("~");        
+    }
+
+    Book.prototype.dump = function () {
+        this.root.dump();
+    }
+
+    return Book;
+
+})(ContactBook.Contact, ContactBook.Group);
---
+++ Labs/01 OOP/Solution/Test.js
@@ -0,0 +1,25 @@
+﻿(function (Book, Group, Contact) {
+
+    function seed(book) {
+        var friends = new Group("Friends");
+        book.root.addItem(friends);
+
+        var coWorkers = new Group("Co-Workers");
+        book.root.addItem(coWorkers);
+
+        var ori = new Contact("Ori", "ori@gmail.com");
+        book.root.addItem(ori);
+
+        var udi = new Contact("Udi", "udi@gmail.com");
+        friends.addItem(udi);
+
+        var shlomi = new Contact("Shlomi", "shlomi@gmail.com");
+        coWorkers.addItem(shlomi);
+    }
+
+    var book = new Book();
+    seed(book);
+
+    book.dump();
+
+})(ContactBook.Book, ContactBook.Group, ContactBook.Contact);
