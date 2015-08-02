757cbacdefb5eea91377eebbde5ffce675cdbc37
oricalvo <ori.calvo@gmail.com>
2015-08-02 16:16:19 +0300

    More

---
+++ Labs/02 Introducing Angular/Solution/App.js
@@ -0,0 +1,24 @@
+﻿(function (book, Group, Contact) {
+
+    angular.module("ContactBookApp", []).run(function () {
+        function seed(book) {
+            var friends = new Group("Friends");
+            book.root.addItem(friends);
+
+            var coWorkers = new Group("Co-Workers");
+            book.root.addItem(coWorkers);
+
+            var ori = new Contact("Ori", "ori@gmail.com");
+            book.root.addItem(ori);
+
+            var udi = new Contact("Udi", "udi@gmail.com");
+            friends.addItem(udi);
+
+            var shlomi = new Contact("Shlomi", "shlomi@gmail.com");
+            coWorkers.addItem(shlomi);
+        }
+
+        seed(book);
+    });
+
+})(ContactBook.book, ContactBook.Group, ContactBook.Contact);
---
+++ Labs/02 Introducing Angular/Solution/Models/BookItem.js
@@ -0,0 +1,15 @@
+﻿var ContactBook = ContactBook || {};
+
+ContactBook.BookItem = (function () {
+
+    function BookItem() {
+        this.parent = null;
+    }
+
+    BookItem.prototype.onAdded = function (parent) {
+        this.parent = parent;
+    }
+
+    return BookItem;
+
+})();
