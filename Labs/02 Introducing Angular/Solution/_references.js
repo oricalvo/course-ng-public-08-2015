5014f8fdfaff42ca87c7f9513f52b9b12bc2bd37
oricalvo <ori.calvo@gmail.com>
2015-08-05 11:09:15 +0300

    More

--- Labs/02 Introducing Angular/Solution/_references.js
+++
@@ -1,3 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="angular.js" />
-/// <reference path="jquery.js" />
--- Labs/04 Enhancing Views/Starter/Angular/Scripts/Services/App.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-
-    angular.module("MyApp", [])
-        .config(function () {
-        })
-        .run(function () {
-        });
-
-})();
