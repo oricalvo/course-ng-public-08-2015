95d4f86a2a9269c737cea7095e50df9c131b2c16
oricalvo <ori.calvo@gmail.com>
2015-08-05 13:50:59 +0300

    More

---
+++ Labs/02 Introducing Angular/Solution/Book.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="Group.js" />
+
+var ContactBook = ContactBook || {};
+
+ContactBook.book = (function (Contact, Group) {
+
+    function Book() {
+        this.root = new Group("~");
+
+        this.seed(this.root);
+    }
+
+    Book.prototype.dump = function () {
+        this.root.dump();
+    }
+
+    Book.prototype.seed = function(root) {
+        var friends = new Group("Friends");
+        root.addItem(friends);
+
+        var coWorkers = new Group("Co-Workers");
+        root.addItem(coWorkers);
+
+        var ori = new Contact("Ori", "ori@gmail.com");
+        root.addItem(ori);
+
+        var udi = new Contact("Udi", "udi@gmail.com");
+        friends.addItem(udi);
+
+        var shlomi = new Contact("Shlomi", "shlomi@gmail.com");
+        coWorkers.addItem(shlomi);
+    }
+
+    var book = new Book();
+    book.dump();
+
+    return book;
+
+})(ContactBook.Contact, ContactBook.Group);
---
+++ Labs/02 Introducing Angular/Solution/GroupIndexCtrl.js
@@ -0,0 +1,22 @@
+﻿(function (Group) {
+
+    function GroupIndexCtrl($scope) {
+
+        $scope.group = ContactBook.book.root;
+
+        $scope.changeCurrentGroup = function (item) {
+            if (item instanceof Group) {
+                $scope.group = item;
+            }
+        }
+
+        $scope.gotoParent = function () {
+            if ($scope.group.parent) {
+                $scope.group = $scope.group.parent;
+            }
+        }
+    }
+
+    angular.module("MyApp").controller("GroupIndexCtrl", GroupIndexCtrl);
+
+})(ContactBook.Group);
